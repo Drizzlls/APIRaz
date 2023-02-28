5f1e09a6fd3660418326466f7095a432656a75d9
Drizzlls <drizzlls@yandex.ru>
2023-02-28 15:39:03 +0300

    добавил запуск бп

--- apiServiceRazdorov/dataCustomerInTgBot/userGetMoney.py
+++ apiServiceRazdorov/dataCustomerInTgBot/userGetMoney.py
@@ -8,6 +8,7 @@ class GetMoney:
         self.nickname = nickname
         self.chatId = chatId
 
+
     def getUser(self):
 
         findForChatId = self.getForChatId()
@@ -39,17 +40,21 @@ class GetMoney:
                                                                                    fields={
                                                                                        'UF_CRM_1673863688405': datetime.now().date()
                                                                                    })
+                            startProcess = self.startBusinessProcess(chat['ID'])
+
                             return 'Ok'
                         elif chat.get('UF_CRM_1673863435606', None) == '25992': # Должник
                             updateDeal = deal = Bitrix24DataTgInfoBot.B.callMethod('crm.deal.update', ID=chat['ID'],
                                                                                    fields={
                                                                                        'UF_CRM_1674592190066': datetime.now().date()
                                                                                    })
+                            startProcess = self.startBusinessProcess(chat['ID'])
+
                             return 'Ok'
                         elif chat.get('UF_CRM_1673863435606', None) == '26096': # Нет официального дохода
                             return 'Ошибка запроса , обратитесь к своему сопровождающему'
                         else:
-                            return 'Невозможно запросить снятие из-за нехватки данных , обратитесь пожалуйста к Вашему сопровождающему'
+                            return 'Невозможно запросить снятие из-за нехватки данных, обратитесь, пожалуйста, к Вашему сопровождающему'
                     else:
                         return 'Прошлый процесс снятия не закрыт , обратитесь к своему сопровождающему'
         return False
@@ -74,19 +79,26 @@ class GetMoney:
                                                                                    fields={
                                                                                        'UF_CRM_1673863688405': datetime.now().date()
                                                                                    })
+                            startProcess = self.startBusinessProcess(nick['ID'])
+
                             return 'Ok'
                         elif nick.get('UF_CRM_1673863435606', None) == '25992':  # Должник
                             updateDeal = deal = Bitrix24DataTgInfoBot.B.callMethod('crm.deal.update', ID=nick['ID'],
                                                                                    fields={
                                                                                        'UF_CRM_1674592190066': datetime.now().date()
                                                                                    })
+                            startProcess = self.startBusinessProcess(nick['ID'])
+
                             return 'Ok'
                         elif nick.get('UF_CRM_1673863435606', None) == '26096': # Нет официального дохода
                             return 'Ошибка запроса , обратитесь к своему сопровождающему'
                         else:
-                            return 'Невозможно запросить снятие из-за нехватки данных , обратитесь пожалуйста к Вашему сопровождающему'
+                            return 'Невозможно запросить снятие из-за нехватки данных, обратитесь, пожалуйста, к Вашему сопровождающему'
 
                     return 'Прошлый процесс снятия не закрыт , обратитесь к своему сопровождающему'
-
         return False
 
+    def startBusinessProcess(self, id):
+        start = Bitrix24DataTgInfoBot.B.callMethod('bizproc.workflow.start', TEMPLATE_ID=1056,
+                                                   DOCUMENT_ID=['crm', 'CCrmDocumentDeal', id])
+        return 'Процесс выполнен'
