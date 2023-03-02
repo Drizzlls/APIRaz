f5295d400d0b9a949040222692dec100f90b56df
DrizzllDev <drizzllsvery@yandex.ru>
2023-03-02 18:08:28 +0300

    обновил бебхук

--- apiServiceRazdorov/bitrixTask/classBitrix.py
+++ apiServiceRazdorov/bitrixTask/classBitrix.py
@@ -2,15 +2,15 @@ from bitrix24 import *
 
 
 class Bitrix24Data:
-    WEBHOOK = "https://novoedelo.bitrix24.ru/rest/16/1afcxi0dvbixto70/"
+    WEBHOOK = "https://novoedelo.bitrix24.ru/rest/16/4a57eux5cbpp33qs/"
     """ Тестовый """
     # WEBHOOK = "https://novoedelo.bitrix24.ru/rest/16/cpr67qfdl69mk1db/"
     B = Bitrix24(WEBHOOK)
 
 class Bitrix24DataTgBot:
-    WEBHOOK = "https://novoedelo.bitrix24.ru/rest/16/rvwbud31d1uqh6hg/"
+    WEBHOOK = "https://novoedelo.bitrix24.ru/rest/16/4a57eux5cbpp33qs/"
     B = Bitrix24(WEBHOOK)
 
 class Bitrix24DataTgInfoBot:
-    WEBHOOK = "https://novoedelo.bitrix24.ru/rest/16/xncdiq6cdpsetre9/"
+    WEBHOOK = "https://novoedelo.bitrix24.ru/rest/16/4a57eux5cbpp33qs/"
     B = Bitrix24(WEBHOOK)
